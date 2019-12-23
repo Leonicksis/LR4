eb3da3e721bb41f54e3821d2cac54e06838135ba
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2019-12-23 12:55:16 +0300

    Update 20191124190908_create_artcles.rb

--- db/migrate/20191124190908_create_artcles.rb
+++ db/migrate/20191124190908_create_artcles.rb
@@ -4,8 +4,8 @@ class CreateArtcles < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :description
       t.string :url
-      t.references :author, null: false, foreign_key: true
-      t.references :blog, null: false, foreign_key: true
+      t.references :creator, null: false, foreign_key: true
+      t.references :creature, null: false, foreign_key: true
 
       t.timestamps
     end
