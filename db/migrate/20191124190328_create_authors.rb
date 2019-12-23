e37759976b855057d11a70388740f2deafd743fe
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2019-12-23 12:53:08 +0300

    Update 20191124190328_create_authors.rb

--- db/migrate/20191124190328_create_authors.rb
+++ db/migrate/20191124190328_create_authors.rb
@@ -1,8 +1,8 @@
-class CreateAuthors < ActiveRecord::Migration[6.0]
+class CreateCreator < ActiveRecord::Migration[6.0]
   def change
-    create_table :authors do |t|
-      t.string :first_name
-      t.string :last_name
+    create_table :creators do |t|
+      t.string :imya
+      t.string :familia
 
       t.timestamps
     end
