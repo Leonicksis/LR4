78dd910b0d8273f4493bf3cb83e66ee4b5e83c9b
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2019-12-23 12:56:20 +0300

    Update 20191124190908_create_art.rb

--- db/migrate/20191124190908_create_art.rb
+++ db/migrate/20191124190908_create_art.rb
@@ -1,6 +1,6 @@
 class CreateArtcles < ActiveRecord::Migration[6.0]
   def change
-    create_table :artcles do |t|
+    create_table :art do |t|
       t.string :title
       t.string :description
       t.string :url
