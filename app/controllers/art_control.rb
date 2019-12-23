43ba8f7c5f141bd00e8740eb53b60cf07adb5ade
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2019-12-23 12:33:19 +0300

    Update and rename artcles_controller.rb to art_control.rb

--- app/controllers/art_control.rb
+++ app/controllers/art_control.rb
@@ -1,51 +1,51 @@
-class ArtclesController < ApplicationController
+class ArtControl < AppControl
   before_action :set_artcle, only: [:show, :update, :destroy]
 
   # GET /artcles
-  def index
-    @artcles = Artcle.all
+  def idi
+    @arts = Artcle.all
 
-    render json: @artcles
+    render json: @arts
   end
 
   # GET /artcles/1
   def show
-    render json: @artcle
+    render json: @arts
   end
 
   # POST /artcles
   def create
-    @artcle = Artcle.new(artcle_params)
+    @arts = Artcle.new(arts_par)
 
-    if @artcle.save
-      render json: @artcle, status: :created, location: @artcle
+    if @arts.save
+      render json: @arts, status: :created, location: @arts
     else
-      render json: @artcle.errors, status: :unprocessable_entity
+      render json: @arts.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /artcles/1
   def update
-    if @artcle.update(artcle_params)
-      render json: @artcle
+    if @arts.update(arts_par)
+      render json: @arts
     else
-      render json: @artcle.errors, status: :unprocessable_entity
+      render json: @arts.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /artcles/1
   def destroy
-    @artcle.destroy
+    @arts.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_artcle
-      @artcle = Artcle.find(params[:id])
+      @arts = Artcle.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
-    def artcle_params
+    def arts_par
       params.require(:artcle).permit(:title, :description, :url, :author_id, :blog_id)
     end
 end
