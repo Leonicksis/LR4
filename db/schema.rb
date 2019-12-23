a59fa253e862d808f039e53c38a73cedc7c2cd89
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2019-12-23 12:51:51 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+
 
 ActiveRecord::Schema.define(version: 2019_11_24_190908) do
 
@@ -19,27 +9,27 @@ ActiveRecord::Schema.define(version: 2019_11_24_190908) do
     t.string "title"
     t.string "description"
     t.string "url"
-    t.bigint "author_id", null: false
-    t.bigint "blog_id", null: false
+    t.bigint "creator_id", null: false
+    t.bigint "creature_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_artcles_on_author_id"
-    t.index ["blog_id"], name: "index_artcles_on_blog_id"
+    t.index ["creator_id"], name: "index_artcles_on_creator_id"
+    t.index ["creature_id"], name: "index_artcles_on_creature_id"
   end
 
-  create_table "authors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table "creators", force: :cascade do |t|
+    t.string "Imya"
+    t.string "Familia"
+    t.datetime "created", precision: 6, null: false
+    t.datetime "updated", precision: 6, null: false
   end
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "creatures", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created", precision: 6, null: false
+    t.datetime "updated", precision: 6, null: false
   end
 
-  add_foreign_key "artcles", "authors"
-  add_foreign_key "artcles", "blogs"
+  add_foreign_key "artcles", "creators"
+  add_foreign_key "artcles", "creatures"
 end
