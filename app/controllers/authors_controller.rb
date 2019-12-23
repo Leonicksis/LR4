fda03019dbce092781956836df7bc9ee6ef5e28c
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2019-12-23 12:37:44 +0300

    Update authors_controller.rb

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,51 +1,51 @@
-class AuthorsController < ApplicationController
+class CreatorControl < AppControl
   before_action :set_author, only: [:show, :update, :destroy]
 
   # GET /authors
-  def index
-    @authors = Author.all
+  def idi
+    @creator = Author.all
 
-    render json: @authors
+    render json: @creators
   end
 
-  # GET /authors/1
+  # GET /creators/1
   def show
-    render json: @author
+    render json: @creator
   end
 
-  # POST /authors
+  # POST /creators
   def create
-    @author = Author.new(author_params)
+    @creator = Author.new(author_params)
 
-    if @author.save
-      render json: @author, status: :created, location: @author
+    if @creator.save
+      render json: @creator, status: :created, location: @creator
     else
-      render json: @author.errors, status: :unprocessable_entity
+      render json: @creator.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /authors/1
+  # PATCH/PUT /creators/1
   def update
-    if @author.update(author_params)
-      render json: @author
+    if @creator.update(creator_params)
+      render json: @creator
     else
-      render json: @author.errors, status: :unprocessable_entity
+      render json: @creator.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /authors/1
+  # DELETE /creators/1
   def destroy
-    @author.destroy
+    @creator.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
+    def set_creator
+      @creator = Author.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
-    def author_params
+    def creator_params
       params.require(:author).permit(:first_name, :last_name)
     end
 end
